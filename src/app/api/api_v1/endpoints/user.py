3ef9033bb58a621c2482cd343bdf2ad9d495399a
CRI USER <sandragjacinto@gmail.com>
2025-09-15 11:05:04 +0200

    adds session endpoint

--- src/app/api/api_v1/endpoints/user.py
+++ src/app/api/api_v1/endpoints/user.py
@@ -8,8 +8,24 @@ from src.app.utils.logger import logger as logger_utils
 router = APIRouter()
 logger = logger_utils(__name__)
 
-@router.post("/user", summary="creates new user", description="Create a new user in the user db", response_model=dict)
-async def create_user():
+@router.get("/user", summary="creates new user", description="Create a new user in the user db", response_model=dict)
+async def handle_user():
     try:
+        # check if id and if id in table
         # put new line in user db
         # return user id
+        return {"user_id": "user_id"}
+    except Exception as e:
+        logger.error(f"Error creating user: {e}")
+
+
+@router.get("/session", summary="creates new session", description="Create a new session in the user db", response_model=dict)
+async def handle_session():
+    try:
+        # check if id and if id in table
+        # check if end_at is still more recent than time now
+        # put new line in session db
+        # return session id
+        return {"session_id": "session_id"}
+    except Exception as e:
+        logger.error(f"Error creating session: {e}")
