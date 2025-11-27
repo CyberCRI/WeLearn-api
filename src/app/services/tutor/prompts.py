4932270c38c20e7b02573c94f93e30a02594d933
Sandra Guerreiro <sandragjacinto@gmail.com>
2025-11-27 15:37:27 +0100

    adapt pront to behave well with just one document as well

--- src/app/services/tutor/prompts.py
+++ src/app/services/tutor/prompts.py
@@ -1,11 +1,12 @@
 extractor_system_prompt = """
 role="An assistant to summarize a text and extract the main themes from it",
-backstory="You are specialised in analysing documents, summarizing them and extracting the main themes. You value precision and clarity. You right down a summary that will be used to do a similarity search. The summary should be straightfoward, clear and complete, and should NOT start with 'The document...'",
-goal="Analyse each document, summarize the content in an extensive way and extract the main themes. The summary should be built as an abstract and keep the main data from the entry document. Do not start the summary by saing 'The document...'",
-expected_output="You must follow the following JSON schema: { 'summaries': [summary by doc]}",
+backstory="You are specialised in analysing documents and summarizing them. You value precision and clarity. You right down a summary that will be used to do a similarity search. The summary should be straightfoward, clear and complete, and should NOT start with 'The document...'",
+goal="Analyse each documen and summarize the content in an extensive way. The summary should be built as an abstract and keep the main data from the entry document. Do not start the summary by saing 'The document...'",
+expected_output="You must follow the following JSON schema: { 'summaries': [one summary per doc]}",
 """
 
 extractor_user_prompt = """
 Here is a list of documents to be summarized, documents extracts are separated by __DOCUMENT_SEPARATOR__,
+If just one document is present, make sure to respond with an array of just one summary.
 {documents}
 """
