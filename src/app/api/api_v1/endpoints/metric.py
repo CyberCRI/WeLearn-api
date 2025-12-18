3d1d25a6ccc11b38d706e57bafe5110ee1454244
Théo <theo.nardin@cri-paris.org>
2025-12-18 17:38:09 +0100

    feat(metric): add metric endpoint to retrieve document quantity per corpus

---
+++ src/app/api/api_v1/endpoints/metric.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter
+from starlette.concurrency import run_in_threadpool
+
+from src.app.api.dependencies import get_settings
+from src.app.models.metric import RowCorpusQtyDocInfo
+from src.app.services.sql_db.queries import get_document_qty_table_info_sync
+from src.app.utils.logger import logger as utils_logger
+
+logger = utils_logger(__name__)
+
+router = APIRouter()
+
+settings = get_settings()
+
+
+@router.get("/nb_docs_info_per_corpus")
+async def get_nb_docs_info_per_corpus() -> list[RowCorpusQtyDocInfo | None]:
+    result = await run_in_threadpool(get_document_qty_table_info_sync)
+    ret = []
+    if not result:
+        return []
+    for r_corpus, r_qty_doc_in_qdrant, r_qty_doc_total in result:
+        current = RowCorpusQtyDocInfo(
+            corpus=r_corpus.source_name,
+            url=r_corpus.main_url,
+            qty_total=r_qty_doc_total.count,
+            qty_in_qdrant=r_qty_doc_in_qdrant.count,
+        )
+        ret.append(current)
+
+    return ret
