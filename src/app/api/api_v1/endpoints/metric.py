51e30ba46119a01a366033d1685b3ee41f7dc892
Théo <133012334+lpi-tn@users.noreply.github.com>
2026-01-08 10:53:39 +0100

    Update src/app/api/api_v1/endpoints/metric.py
    Co-authored-by: Sandra Guerreiro  <sandragjacinto@gmail.com>

--- src/app/api/api_v1/endpoints/metric.py
+++ src/app/api/api_v1/endpoints/metric.py
@@ -16,9 +16,10 @@ settings = get_settings()
 @router.get("/nb_docs_info_per_corpus")
 async def get_nb_docs_info_per_corpus() -> list[RowCorpusQtyDocInfo | None]:
     result = await run_in_threadpool(get_document_qty_table_info_sync)
-    ret = []
     if not result:
         return []
+    
+    ret = []
     for r_corpus, r_qty_doc_in_qdrant, r_qty_doc_total in result:
         current = RowCorpusQtyDocInfo(
             corpus=r_corpus.source_name,
