e534d1395a6f06d2f9296fe9dc6651e02b1ea1b8
Théo <theo.nardin@cri-paris.org>
2026-01-08 11:48:30 +0100

    lint

--- src/app/api/api_v1/endpoints/metric.py
+++ src/app/api/api_v1/endpoints/metric.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
+from pydantic import ValidationError
 from starlette.concurrency import run_in_threadpool
 
 from src.app.api.dependencies import get_settings
@@ -14,19 +15,26 @@ settings = get_settings()
 
 
 @router.get("/nb_docs_info_per_corpus")
-async def get_nb_docs_info_per_corpus() -> list[RowCorpusQtyDocInfo | None]:
+async def get_nb_docs_info_per_corpus(
+    response: Response,
+) -> list[RowCorpusQtyDocInfo | None]:
     result = await run_in_threadpool(get_document_qty_table_info_sync)
     if not result:
         return []
 
     ret = []
     for r_corpus, r_qty_doc_in_qdrant, r_qty_doc_total in result:
-        current = RowCorpusQtyDocInfo(
-            corpus=r_corpus.source_name,
-            url=r_corpus.main_url,
-            qty_total=r_qty_doc_total.count,
-            qty_in_qdrant=r_qty_doc_in_qdrant.count,
-        )
-        ret.append(current)
-
+        try:
+            current = RowCorpusQtyDocInfo(
+                corpus=r_corpus.source_name,
+                url=r_corpus.main_url,
+                qty_total=r_qty_doc_total.count,
+                qty_in_qdrant=r_qty_doc_in_qdrant.count,
+            )
+            ret.append(current)
+        except ValidationError as e:
+            response.status_code = status.HTTP_206_PARTIAL_CONTENT
+            logger.error(f"Validation error for corpus {r_corpus.source_name}: {e}")
+    if len(ret) == 0:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     return ret
