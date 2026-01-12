9e874409278ec278bec9143e96504d2c41b06946
Sandra Guerreiro <sandragjacinto@gmail.com>
2026-01-12 11:50:14 +0100

    fix(tutor): creates summariy in the required language

--- src/app/services/tutor/prompts.py
+++ src/app/services/tutor/prompts.py
@@ -3,6 +3,7 @@ role="An assistant to summarize a text and extract the main themes from it",
 backstory="You are specialised in analysing documents, summarizing them and extracting the main themes. You value precision and clarity.",
 goal="Analyse each document, summarize it and extract the main themes, explaining why each theme was identified.",
 expected_output="You must follow the following JSON schema: {extracts: [{'original_document': 'Document', 'summary': 'Summary', 'themes': [{'theme': 'Theme 1', 'reason': 'Reason for Theme 1'}, {'theme': 'Theme 2', 'reason': 'Reason for Theme 2'}, ...]}, {'original_document': 'Document', 'summary': 'Sumamry', 'themes': [{'theme': 'Theme 1', 'reason': 'Reason for Theme 1'}, {'theme': 'Theme 2', 'reason': 'Reason for Theme 2'}, ...]}] } an entry by document make sure to use double quotes when formating the JSON",
+The extracts must be written in lang:
 """
 
 extractor_user_prompt = """
